9e5a272d1f9afe9deda060c7fb7209bc57e6f1d4
jiqi <sunqqxp@hotmail.com>
2019-03-29 10:45:35 +0000

    first commit

---
+++ dapp/truffle.js
@@ -0,0 +1,35 @@
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() { 
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
+
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'spirit supply whale amount human item harsh scare congress discover talent hamster';
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: "*"
+    }, 
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/551dccdfe6494700951e6d806fd22d87') 
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+  }
+};
